7b0abb492796ea09cb8b041b9991bda2c263fa90
Muhammad Umar Farooq <92017762+UmarFarooq361@users.noreply.github.com>
2022-10-13 23:10:44 +0500

    Day1 - printing , declaring variables , inputs , concatinations

---
+++ Day1/BrandNameGenerator.py
@@ -0,0 +1,4 @@
+# declaring city and country variable and store values in it
+city =  input("What is the name of your city ? \n")
+country = input("What is name of your country ? \n")
+print("Your location is "+ city + " , "+ country)
