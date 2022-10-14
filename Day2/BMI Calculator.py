eea4688875de369bd283f234d62a7901eb6ba72b
Muhammad Umar Farooq <92017762+UmarFarooq361@users.noreply.github.com>
2022-10-14 22:23:23 +0500

    Day2 - Check your BMI

---
+++ Day2/BMI Calculator.py
@@ -0,0 +1,8 @@
+height =  input("What is your height in meter ? ")
+weight = input("What is weigth in kg ? ")
+height_as_int = float(height)
+weight_as_int = float(weight)
+
+IMB = weight_as_int/height_as_int** 2
+
+print(int(IMB))
