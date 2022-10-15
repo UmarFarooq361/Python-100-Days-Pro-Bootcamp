023e6aba53e879b4cb6614ee74d8c6ed5a3611da
Muhammad Umar Farooq <92017762+UmarFarooq361@users.noreply.github.com>
2022-10-16 00:20:14 +0500

    conditional statement

---
+++ Day3/3.2 rider trip.py
@@ -0,0 +1,17 @@
+# practice of conditions like if elif else
+num = int(input("Enter your height ? "))
+
+if num>=120:
+    age = int(input("Enter your age ? "))
+    if age < 12:
+       print("You have to pay $2")
+    elif age <=18:
+        print("You have to pay $5")
+    else:
+        print("You have to pay $8")
+
+else:
+    print("Your are not allowed to ride")
+
+
+
