cd765c2e6f51f3914d932423c047c47e6aca652d
Muhammad Umar Farooq <92017762+UmarFarooq361@users.noreply.github.com>
2022-10-16 00:19:28 +0500

    simple pizza bill generator

---
+++ Day3/3.4 Pizza Order.py
@@ -0,0 +1,22 @@
+print("Welcome to Pizza Order.")
+size= (input("What is the size of your pizza ? S , M , L ?"))
+add_pepperoni= (input("Do you wont to add extra pepperoni ? Y , N ?"))
+extra_cheez= (input("Do you wont to add extra cheez ? Y , N ?"))
+bill = 0
+
+if (size=="S"):
+    bill += 15
+elif (size=="M"):
+    bill += 20
+else:
+    bill += 25
+
+if size=="S":
+    if add_pepperoni == "Y":
+        bill += 2
+else:
+    bill+=3
+if extra_cheez == "Y":
+    bill += 1
+
+print(f"Your total bill is ${bill}")
