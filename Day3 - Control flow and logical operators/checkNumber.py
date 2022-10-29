0e0abc799bd2a08284cc1fa17a4b0f41ed7e700a
Muhammad Umar Farooq <92017762+UmarFarooq361@users.noreply.github.com>
2022-10-29 08:55:57 +0500

    check number is even or odd

--- Day3 - Control flow and logical operators/checkNumber.py
+++ Day3 - Control flow and logical operators/checkNumber.py
@@ -1,6 +1,6 @@
 #assisment 3.1 : check number is even or odd
 number = int(input("Enter any number to check its even or odd ? "))
-if (number%2==0):
+if (number%2 == 0):
     print("Number is even .")
 else:
     print("Number is odd .")
