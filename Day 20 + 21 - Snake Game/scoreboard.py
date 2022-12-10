fc4fd182fffe54a1cf4f3225042dce0449ad8917
Muhammad Umar Farooq <92017762+UmarFarooq361@users.noreply.github.com>
2022-12-10 21:41:51 +0500

    adding reset class to play again and again

--- Day 20 + 21 - Snake Game/scoreboard.py
+++ Day 20 + 21 - Snake Game/scoreboard.py
@@ -4,7 +4,9 @@ FONT = ("arial" , 20 , "normal")
 class Scoreboard(Turtle):
     def __init__(self):
         super(Scoreboard, self).__init__()
-        self.SCORE = 0
+        with open("data.txt") as data:
+            self.highscore = int(data.read())
+        self.score = 0
         self.color("white")
         self.penup()
         self.goto(0 , 270)
@@ -12,13 +14,21 @@ class Scoreboard(Turtle):
         self.update_score()
 
     def update_score(self):
-        self.write(f"Score: {self.SCORE}", align=ALIGNMENT, font= FONT)
-    def increase_score(self):
-        self.SCORE +=1
         self.clear()
+        self.write(f"Score: {self.score} ~ High Score: {self.highscore}", align=ALIGNMENT, font= FONT)
+    def increase_score(self):
+        self.score +=1
         self.update_score()
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write("Game Over!", align="center", font= FONT)
+    def reset(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+            with open("data.txt", mode="w")as data:
+                data.write(f"{self.highscore}")
+        self.score = 0
+        self.update_score()
+
+    # def game_over(self):
+    #     self.goto(0,0)
+    #     self.write("Game Over!", align="center", font= FONT)
 
