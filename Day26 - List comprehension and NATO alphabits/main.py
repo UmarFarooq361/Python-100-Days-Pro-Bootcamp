7e60ab34f4472530ae872c1bf65346bf289a971b
Muhammad Umar Farooq <92017762+UmarFarooq361@users.noreply.github.com>
2022-12-24 20:37:18 +0500

    modified version :handle key errors using error handling

--- Day26 - List comprehension and NATO alphabits/main.py
+++ Day26 - List comprehension and NATO alphabits/main.py
@@ -1,10 +1,14 @@
 import pandas
 
-#TODO 1. Create a dictionary in this format:
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 data_dict = {row.letter:row.code for (index, row) in data.iterrows()}
-
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-name = input("Enter your name: ").upper()
-nato_name = [data_dict[letter] for letter in name]
-print(nato_name)
+err = True
+while err:
+    name = input("Enter your name: ").upper()
+    try:
+        nato_name = [data_dict[letter] for letter in name]
+    except KeyError:
+        print("Sorry enter only alphabets please")
+    else:
+        print(nato_name)
+        err = False
